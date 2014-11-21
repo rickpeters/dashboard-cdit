3ad276d0b08154ffb20659c4e3c61a23f1417ab8
rickpeters <rick.peters@me.com>
2014-11-21 13:27:44 +0100

    Nieuwe multicast versie met carousel

--- js/cdit.js
+++ js/cdit.js
@@ -1,8 +1,16 @@
 var globalseries = {};
+//var PDOKAgileSeries = {};
+//var PDOKCDSeries = {};
+//var BRKBAGAgileSeries = {};
+//var BRKBAGCDSeries = {};
 
 var globaloptions = {
 	series: [],
-
+	
+	title: {
+		text: 'Continuous Delivery',
+		verticalAlign: 'top'
+    },
 	chart: {
 		polar: true,
 		// type: 'line',
@@ -14,38 +22,41 @@ var globaloptions = {
 	credits: {
 		enabled: false
 	},
-	title: {
-	// 	text: id
-	},
+	
 
 	pane: {
-		size: '80%'
+		size: '70%'
 	},
 
 	legend: {
 		reversed: false,
 		enabled: false,
-		align: 'right',
-		verticalAlign: 'top',
-		y: 100,
-		layout: 'vertical'
+		align: 'center',
+		verticalAlign: 'bottom',
+		layout: 'horizontal'
 	},
 
 	xAxis: {
 		tickmarkPlacement: 'between',
-		//categories: ['Build', 'Test', 'Delivery', 'Pipeline', 'XaaS']
+		//categories: ['continuous integration', 'automated deployment', 'test automation', 'provisioning', 'architecture']
 		categories: []
 	},
 
 	yAxis: {
 		min: 0,
 		max: 100,
+		tickInterval: 20,
 		endOnTick: false,
 		showLastLabel: false,
-		title: {
-			text: 'score (%)'
-		},
+		//title: {
+		//	text: 'score (%)'
+		//},
 		labels: {
+			enabled: true,
+			style: {
+				color: '#FFFFFF'
+			},
+			//step: 20,
 			formatter: function() {
 				return this.value + '%';
 			}
@@ -65,11 +76,19 @@ var globaloptions = {
 
 	plotOptions: {
 		series: {
-			stacking: null,
+			stacking: null, //'normal', //'normal', //null, normal, percent
 			shadow: false,
 			groupPadding: 0,
-			pointPlacement: 'on'
-		}
+			pointPlacement: 'on',
+			dataLabels: {
+				enabled: false,
+				color: '#FFFFFF'
+			}
+		},
+		column: {
+                	grouping: true,
+                	shadow: false
+                }
 	}
 };
 
@@ -77,79 +96,84 @@ var globaloptions = {
 // {{placeholder_js}}
 var paletcolor = ['#BCCF02', '#5BB12F', '#9B539C', '#EB65A0', '#73C5E1', '#fb7676','#6ef96e','#4e4ef8','#f9d961','#325da7', '#f68c06', '#c19e9e', '#a8c5a8'];
 
-function polarchart(parseries) {
+//function polarchart(parseries) {
 
 	// create a deep copy of globaloptions
-	var options = $.extend(true, {}, globaloptions);
+//	var options = $.extend(true, {}, globaloptions);
 	
 	// add series to options
-	options.series.push(parseries);
+//	options.series.push(parseries);
 
-	options.title.text = parseries.name;	
+//	options.title.text = parseries.name;	
 	
-	$('#' + parseries.name).highcharts(options);
+//	$('#' + parseries.name).highcharts(options);
 
-	return;
-}
+//	return;
+//}
 
-function initdata(){
+function initdata(projectId, scanType){
 	
-		$.ajax({ url: '/js/data.csv', 
+	//var copiedData = 'scan,ci,ad,ta,p,a$target, 10, 0, 10, 20, 0$final, 40, 50, 40, 20, 10$intermediate, 10, 20, 0, 30, 10$inital, 30, 10, 20, 10, 50';
+	//var copiedData = 'scan,ci,ad,ta,p,a$target, 80, 80, 60, 80, 60$final, 70, 80, 50, 60, 50$intermediate, 40, 60, 40, 40, 20$inital, 20, 40, 10, 20, 10';
+	//var lines = copiedData.split('$');
+    //var projectId = 'data';
+	$.ajax({ url: '/scans/js/' + projectId + '-' + scanType + '-data.csv', 
 	    		 async: false,
 				 dataType: 'text',
 	         	 success: function(data) {
-			
-		//$.get('js/data.csv', function(data) {
-	    // Split the lines
-	    var lines = data.split('\n');
-    
-		var curLine = 0;
-	    // Iterate over the lines and add categories or series
-	    $.each(lines, function(lineNo, line) {
-			
-	        var items = line.split(',');
-			// skip empty, comment and wrong  lines
-			if (line == '' || line.trim().slice(0,2) == '//') return true;
+				 
+	var lines = data.split('\n');			 
+	
+	var curLine = 0;
+	$.each(lines, function(lineNo, line) {
+		var items = line.split(',');
+		// skip empty, comment and wrong  lines
+		if (line == '' || line.trim().slice(0,2) == '//') return true;
         
-	        // header line containes categories
-	        if (curLine == 0) {
-	            $.each(items, function(itemNo, item) {
-	                if (itemNo > 0) globaloptions.xAxis.categories.push(item.trim());
-	            });
-	        }
+	    // header line containes categories
+		if (curLine == 0) {
+		    globaloptions.xAxis.categories = [];
+	        $.each(items, function(itemNo, item) {
+	            if (itemNo > 0) globaloptions.xAxis.categories.push(item.trim());
+	        });
+	    }
         
 	        // the rest of the lines contain data with their name in the first 
 	        // position
-	        else {
-	            var series = {
-	                data: []
+	    else {
+	        var series = {
+	           data: []
 	            };
-	            $.each(items, function(itemNo, item) {
-	                if (itemNo == 0) {
-	                    series.name = item;
-						series.color = paletcolor[curLine - 1];
-						series.fillcolor = series.color;
-	                } else {
-	                    series.data.push(parseFloat(item));
-	                }
-	            });
+	        $.each(items, function(itemNo, item) {
+				if (itemNo == 0) {
+	                series.name = item;
+					series.color = paletcolor[curLine - 1];
+					series.fillcolor = series.color;
+	            } else {
+	                series.data.push(parseFloat(item));
+					series.zIndex = itemNo;
+					series.stack = 0;
+					series.type = 'areaspline';
+	            }
+	        });
             
-	            globalseries[series.name] = series;
+	        globalseries[series.name] = series;
+			
     
-	        }
+	    }
 			// increment real line number (ignoring comments)
-			curLine++;
+		curLine++;
         
-	    });
+	});
     
-	}});	
+}});
+		
 }
 
-function projectchart(id, series){
-
+function agileChart(projectId, id, series){
+	initdata(projectId, 'agile');
 	// create a deep copy of globaloptions
 	var options = $.extend(true, {}, globaloptions);
-	
 	var localseries = [];
 
 	// loop over globalseries and add them to the local series
@@ -158,9 +182,28 @@ function projectchart(id, series){
 	});
 	
 	options.series = localseries;
-	options.title.text = "";
+	options.title.text = "Agile";
+	options.title.align = "center";
 	options.legend.enabled = true;
+	$('#' + projectId + '-' + id).highcharts(options);
+		
+}
+
+function cdChart(projectId, id, series){
+	initdata(projectId, 'cd');
+	// create a deep copy of globaloptions
+	var options = $.extend(true, {}, globaloptions);
+	var localseries = [];
+
+	// loop over globalseries and add them to the local series
+	$.each(series, function(serieNo, serie) {
+		localseries.push(serie);
+	});
 	
-	$('#' + id).highcharts(options);
+	options.series = localseries;
+	options.title.text = "Continuous Delivery";
+	options.title.align = "center";
+	options.legend.enabled = true;
+	$('#' + projectId + '-' + id).highcharts(options);
 		
 }
